7e070ac22f87917e7bfe840b6c10f1ed5a2a8568
AndresFHer <smolie-tj@outlook.com>
2025-05-17 14:01:01 -0500

    Ingrese mas DataFrime a la pagina de Python

--- index.py
+++ index.py
@@ -31,7 +31,24 @@ print(total)
 venta_por_cliente = datos_ventas.groupby('Cliente')['Precio'].mean('Cliente').sum()
 print(venta_por_cliente)
 
+clientes_compras = datos_ventas.groupby('Cliente').size().reset_index(name='Cantidad_de_compras')
+print(clientes_compras)
 
 
+producto_mas_economico = datos_ventas.loc[datos_ventas['Precio'].idxmin()]
+print(producto_mas_economico)
 
 
+productos_vendidos = datos_ventas.groupby('Producto').size().reset_index(name='Cantidad_vendida')
+producto_mas_vendido = productos_vendidos.loc[productos_vendidos['Cantidad_vendida'].idxmax()]
+print(producto_mas_vendido)
+
+productos_por_tienda = datos_ventas.groupby('Tienda').size().reset_index(name='Cantidad_vendida')
+print(productos_por_tienda)
+
+
+precio_promedio_por_tienda = datos_ventas.groupby('Tienda')['Precio'].mean().reset_index(name='Precio_promedio')
+print(precio_promedio_por_tienda)
+
+gasto_por_cliente = datos_ventas.groupby('Cliente')['Precio'].sum().reset_index(name='Gasto_total')
+print(gasto_por_cliente)
